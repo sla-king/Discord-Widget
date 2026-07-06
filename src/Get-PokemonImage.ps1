5e5a4915d57c1e3c1497936a01b3a7c95ee7ec6e
sla-king <alexseidinger15@gmail.com>
2026-07-05 19:59:55 -0700

    Change webp to gif

--- src/Get-PokemonImage.ps1
+++ src/Get-PokemonImage.ps1
@@ -1,6 +1,6 @@
 function Get-PokemonImage {
     $pokeId = Get-Random -Minimum 1 -Maximum 152
-    $pokeImageUrl = "https://raw.githubusercontent.com/sla-king/Discord-Widget/main/assets/$pokeId.webp"
+    $pokeImageUrl = "https://raw.githubusercontent.com/sla-king/Discord-Widget/main/assets/$pokeId.gif"
     return @{
         Id = $pokeId
         Url = $pokeImageUrl
