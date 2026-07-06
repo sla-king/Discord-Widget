1b91ac9260821a0f3f8efdb4d84b7c06ef5fb46c
sla-king <alexseidinger15@gmail.com>
2026-07-05 19:41:28 -0700

    Add webp assets

--- src/Get-PokemonImage.ps1
+++ src/Get-PokemonImage.ps1
@@ -1,6 +1,6 @@
 function Get-PokemonImage {
     $pokeId = Get-Random -Minimum 1 -Maximum 152
-    $pokeImageUrl = "https://raw.githubusercontent.com/sla-king/Discord-Widget/main/assets/$pokeId.png"
+    $pokeImageUrl = "https://raw.githubusercontent.com/sla-king/Discord-Widget/main/assets/$pokeId.webp"
     return @{
         Id = $pokeId
         Url = $pokeImageUrl
